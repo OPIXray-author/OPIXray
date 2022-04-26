f9bb18a91e8e926603d6fe052c48e997c0823175
kenny-seeed <l.w.r.f.42@gmail.com>
2022-04-26 17:04:37 +0800

    triton client

--- DOAM/pth2onnx.py
+++ DOAM/pth2onnx.py
@@ -19,7 +19,7 @@ def Convert_ONNX():
          dummy_input,       # model input (or a tuple for multiple inputs)
          "./weights/ssd.onnx",       # where to save the model
          export_params=True,  # store the trained parameter weights inside the model file
-         opset_version=10,    # the ONNX version to export the model to
+         opset_version=11,    # the ONNX version to export the model to
          do_constant_folding=True,  # whether to execute constant folding for optimization
          input_names = ['modelInput'],   # the model's input names
          output_names = ['modelOutput'], # the model's output names
